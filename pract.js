d4735c33e93b344148e6722428229c166a9212a6
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2024-11-07 10:23:03 -0500

    Add files via upload

---
+++ pract.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", addListener);
+
+function addListener()
+{
+	document.getElementById("btnsubmit").addEventListener("click", UseLocal);
+}
+
+function UseLocal()
+{
+	localStorage.
+}
